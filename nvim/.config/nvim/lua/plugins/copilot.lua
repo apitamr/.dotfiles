0bc33310e60bfa1cca9bcd789b3e3903fd5c3f57
strygwyr <13832826+apitamr@users.noreply.github.com>
2025-11-02 19:16:03 +0700

    chore: adjust config

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,6 +3,7 @@ return {
   event = "InsertEnter",
   config = function()
     -- Copilot settings (keybindings are in lua/config/keymaps.lua)
+    vim.g.copilot_enabled = 0
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
   end,
