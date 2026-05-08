7f322972d34dd93a5b7c3544f77f7ab716efc517
apitamr <13832826+apitamr@users.noreply.github.com>
2026-05-08 11:22:07 +0700

    chore: update config

--- nvim/.config/nvim/lua/plugins/lsp/conform.lua
+++ nvim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -48,6 +48,65 @@ return {
         shfmt = {
           prepend_args = { "-i", "2", "-ci" }, -- 2 spaces, indent switch cases
         },
+        prettier = {
+          command = function(_, ctx)
+            local local_bin = vim.fs.find("node_modules/.bin/prettier", {
+              upward = true,
+              path = ctx.dirname,
+            })[1]
+            return local_bin or "prettier"
+          end,
+        },
+        biome = {
+          command = function(_, ctx)
+            local local_bin = vim.fs.find("node_modules/.bin/biome", {
+              upward = true,
+              path = ctx.dirname,
+            })[1]
+            return local_bin or "biome"
+          end,
+          condition = function(_, ctx)
+            return vim.fs.find({
+              ".prettierrc",
+              ".prettierrc.json",
+              ".prettierrc.yaml",
+              ".prettierrc.yml",
+              ".prettierrc.js",
+              ".prettierrc.cjs",
+              ".prettierrc.mjs",
+              ".prettierrc.toml",
+              "prettier.config.js",
+              "prettier.config.cjs",
+              "prettier.config.mjs",
+            }, { upward = true, path = ctx.dirname })[1] == nil
+          end,
+        },
+        oxfmt = {
+          command = function(_, ctx)
+            local local_bin = vim.fs.find("node_modules/.bin/oxfmt", {
+              upward = true,
+              path = ctx.dirname,
+            })[1]
+            return local_bin or "oxfmt"
+          end,
+          condition = function(_, ctx)
+            return vim.fs.find({
+              ".prettierrc",
+              ".prettierrc.json",
+              ".prettierrc.yaml",
+              ".prettierrc.yml",
+              ".prettierrc.js",
+              ".prettierrc.cjs",
+              ".prettierrc.mjs",
+              ".prettierrc.toml",
+              "prettier.config.js",
+              "prettier.config.cjs",
+              "prettier.config.mjs",
+              "biome.json",
+              "biome.jsonc",
+            }, { upward = true, path = ctx.dirname })[1] == nil
+          end,
+        },
       },
     },
   },
